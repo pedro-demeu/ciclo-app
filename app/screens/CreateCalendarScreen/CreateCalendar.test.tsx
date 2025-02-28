55570b16e29e3d846cded09ad2e9317933932bbb
pdemeu <pdemeu.pessoal@gmail.com>
2025-02-28 00:30:18 -0300

    [Tests] - Refactoring: Update test pattern

---
+++ app/screens/CreateCalendarScreen/CreateCalendar.test.tsx
@@ -0,0 +1,5 @@
+describe("CreateCalendar - Unit Tests", () => {
+  it.todo("when select a date then next button should be visible");
+  it.todo("when no date selected then next button should be hidden");
+  it.todo("when go back pressed then should go to home");
+});
